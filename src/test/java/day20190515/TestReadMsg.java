bc421e8bbf84132d0aa6fb039bb85f22682f5516
susan-you <492308271@qq.com>
2019-05-15 14:18:13 +0800

    test read msg

---
+++ src/test/java/day20190515/TestReadMsg.java
@@ -0,0 +1,42 @@
+package day20190515;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class TestReadMsg {
+
+	@Test(enabled = false)
+	public void readRegisterCode() throws IOException {
+		String msgCodeFilePath = File.separator+"gmall"+File.separator+"logs"+File.separator+"thirdparty-service.log";
+		String msgTemplateId = "d47051c8773343d48136f859689fa7e5|value";
+		String msgCodeRecivePhone = "13694246425";
+		int msgCodeLength = 6;
+		FileReader fReader = new FileReader(msgCodeFilePath);
+		BufferedReader bReader = new BufferedReader(fReader);
+		String LineInfo = "";
+		String matchLine = "";
+		String msgCode = "";
+		int beginIndex = -1;
+		boolean flag = false;
+		while ((LineInfo = bReader.readLine()) != null) {
+			if (LineInfo.indexOf(msgCodeRecivePhone) != -1 && LineInfo.indexOf(msgTemplateId) != -1) {
+				matchLine = LineInfo;
+				beginIndex = LineInfo.indexOf(msgTemplateId);
+				flag = true;
+
+			}
+		}
+		if (flag) {
+			msgCode = matchLine.substring(beginIndex + msgTemplateId.length(),
+					beginIndex + msgTemplateId.length() + msgCodeLength);
+
+			System.out.println(msgCode);
+		}
+
+		bReader.close();
+	}
+}
